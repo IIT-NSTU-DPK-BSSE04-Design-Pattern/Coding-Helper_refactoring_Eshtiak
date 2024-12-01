4865c999d2802c2c91039228ccb9577093725a69
EshtiakJamil <eshtiak.jaamil@gmail.com>
2024-12-01 11:43:27 +0600

    Smell: SPR, HardCodedDimension. Refactor: Building pattern, chart Rendering Logic

--- code_clone/BoxAndWhiskerChart.java
+++ code_clone/BoxAndWhiskerChart.java
@@ -2,14 +2,9 @@ package code_clone;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.EventQueue;
 import java.awt.Font;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 import javax.swing.JFrame;
-import javax.swing.JScrollPane;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.CategoryAxis;
@@ -19,62 +14,47 @@ import org.jfree.chart.renderer.category.BoxAndWhiskerRenderer;
 import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
 
 public class BoxAndWhiskerChart {
-    ChartPanel chartPanel;
 
-    JScrollPane scrollPane;
+    private static final int CHART_WIDTH = 600;
+    private static final int CHART_HEIGHT = 600;
+    private static final Font TITLE_FONT = new Font("SansSerif", Font.BOLD, 20);
 
-    private List<Double> getInputData(double l[]) {
-        ArrayList<Double> list = new ArrayList<>();
-        for (int j = 0; j < l.length; j++) {
-            double d = l[j];
-            //    System.out.println("k=="+l[j]);
-            list.add(l[j]);
+    public void display(List<List<Double>> data, List<String> categories) {
+        DefaultBoxAndWhiskerCategoryDataset dataset = new DefaultBoxAndWhiskerCategoryDataset();
 
-        }//System.out.println("");
-        return list;
+        for (int i = 0; i < data.size(); i++) {
+            dataset.add(data.get(i), "Comparison", categories.get(i));
+        }
+
+        JFreeChart chart = createChart(dataset);
+        showChart(chart);
     }
 
-    public void display() {
-        JFrame f = new JFrame("Clone_Check");
-
-        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        DefaultBoxAndWhiskerCategoryDataset boxData = new DefaultBoxAndWhiskerCategoryDataset();
-
-        for (int i = 0; i < CosineSimilarity.similarArray.size(); i++) {
-            // System.out.println("p="+CosineSimilarity.similarArray.g);
-            boxData.add(getInputData(CosineSimilarity.similarArray.get(i)), "First_Project vs Second_Project", CloneCheck.ProjectFileName1.get(i));
-        }
+    private JFreeChart createChart(DefaultBoxAndWhiskerCategoryDataset dataset) {
         BoxAndWhiskerRenderer renderer = new BoxAndWhiskerRenderer();
         renderer.setFillBox(true);
         renderer.setUseOutlinePaintForWhiskers(true);
-
         renderer.setMedianVisible(true);
         renderer.setMeanVisible(false);
 
-        CategoryAxis xAxis = new CategoryAxis("First_Project_Files");
-        NumberAxis yAxis = new NumberAxis("Second_Project_Values");
-        CategoryPlot plot = new CategoryPlot(boxData, xAxis, yAxis, renderer);
-        final JFreeChart chart = new JFreeChart(
-                "Box-and-Whisker Plot",
-                new Font("SansSerif", Font.BOLD, 20),
-                plot,
-                true
-        );
-        final ChartPanel chartPanel = new ChartPanel(chart);
+        CategoryAxis xAxis = new CategoryAxis("Files");
+        NumberAxis yAxis = new NumberAxis("Similarity (%)");
+
+        CategoryPlot plot = new CategoryPlot(dataset, xAxis, yAxis, renderer);
+
+        JFreeChart chart = new JFreeChart("Box-and-Whisker Plot", TITLE_FONT, plot, true);
         chart.setBackgroundPaint(Color.LIGHT_GRAY);
-        //   JFreeChart chart = new JFreeChart("Test", JFreeChart.DEFAULT_TITLE_FONT, plot, true);
-        f.add(new ChartPanel(chart) {
-            @Override
-            public Dimension getPreferredSize() {
-                return new Dimension(600, 600);
-            }
-        });
-        f.pack();
-        f.setLocationRelativeTo(null);
-        f.setVisible(true);
+        return chart;
     }
 
-    public static void BoxWhisker() {
-        EventQueue.invokeLater(new BoxAndWhiskerChart()::display);
+    private void showChart(JFreeChart chart) {
+        JFrame frame = new JFrame("Box-and-Whisker Chart");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new Dimension(CHART_WIDTH, CHART_HEIGHT));
+        frame.add(chartPanel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
--- code_clone/CosineSimilarity.java
+++ code_clone/CosineSimilarity.java
@@ -3,68 +3,45 @@ package code_clone;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.List;
 
 public class CosineSimilarity {
 
-    public static ArrayList<double[]> similarArray = new ArrayList<double[]>();
-   // public static ArrayList<Double> clone = new ArrayList<>();
+    public List<List<Double>> calculateSimilarity(List<String> fileNames1, List<String> fileNames2) {
+        List<List<Double>> similarityMatrix = new ArrayList<>();
 
-    public void getCosinesimilarity() {
-        double similarity = 0;
-        for (int i = 0; i < CloneCheck.ProjectFileName1.size(); i++) {
-            System.out.println("");
-            double[] similar;
-            //int k = CloneCheck.ProjectFileName2.size();
-            similar = new double[CloneCheck.ProjectFileName2.size()];
-            int count = 0;
-            // similar = new double[TfIdfCalculate.tfidfvectorProject2.size() + 1];
-            for (int j = 0; j < CloneCheck.ProjectFileName2.size(); j++) {
-                similarity = cosineSimilarity(TfIdfCalculate.tfidfvectorProject1.get(i), TfIdfCalculate.tfidfvectorProject2.get(j));
+        for (int i = 0; i < fileNames1.size(); i++) {
+            List<Double> similarities = new ArrayList<>();
+            for (int j = 0; j < fileNames2.size(); j++) {
+                double similarity = cosineSimilarity(TfIdfCalculate.tfidfvectorProject1.get(i),
+                        TfIdfCalculate.tfidfvectorProject2.get(j));
                 BigDecimal bd = new BigDecimal(similarity).setScale(2, RoundingMode.HALF_UP);
-                double getSimilar = bd.doubleValue();
-                System.out.println(CloneCheck.ProjectFileName1.get(i) + " vs " + CloneCheck.ProjectFileName2.get(j) + " " + getSimilar + "%");
-                similar[count] = similarity;
-             //   clone.add(similarity);
-
-                count++;
+                similarities.add(bd.doubleValue());
             }
-            similarArray.add(similar);
+            similarityMatrix.add(similarities);
         }
 
+        return similarityMatrix;
     }
 
-    public double cosineSimilarity(double[] project1, double[] project2) {
-        double dotproduct = 0;
-        double project1magnitude = 0;
-        double project2magnitude = 0;
-        double cosinesimilarity = 0;
-        for (int i = 0; i < project2.length; i++) {
-            dotproduct += project1[i] * project2[i];
-            project1magnitude += Math.pow(project1[i], 2);
-            project2magnitude += Math.pow(project2[i], 2);
+    private double cosineSimilarity(double[] vector1, double[] vector2) {
+        double dotProduct = 0.0;
+        double magnitude1 = 0.0;
+        double magnitude2 = 0.0;
 
+        for (int i = 0; i < vector2.length; i++) {
+            dotProduct += vector1[i] * vector2[i];
+            magnitude1 += Math.pow(vector1[i], 2);
+            magnitude2 += Math.pow(vector2[i], 2);
         }
-        project1magnitude = Math.sqrt(project1magnitude);
-        project2magnitude = Math.sqrt(project2magnitude);
-        //   if(project1magnitude!=0 && project2magnitude!=0){
-        cosinesimilarity = dotproduct / (project1magnitude * project2magnitude) * 100;
-        if (Double.isNaN(cosinesimilarity)) {
-            cosinesimilarity = 0.0;
-        }
-        return cosinesimilarity;
 
+        magnitude1 = Math.sqrt(magnitude1);
+        magnitude2 = Math.sqrt(magnitude2);
+
+        if (magnitude1 == 0 || magnitude2 == 0) {
+            return 0.0;
+        }
+
+        return (dotProduct / (magnitude1 * magnitude2)) * 100;
     }
-
-   /* public void getAverage() {
-        double sum = 0;
-        for (int i = 0; i < clone.size(); i++) {
-          //  System.out.println(clone.get(i));
-            sum += clone.get(i);
-        }
-      
-        double average = sum /clone.size();
-     //   System.out.println(average);
-
-    }*/
-
 }
